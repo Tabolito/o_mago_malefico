eb657d2fc252da1e7eb82a5f44c939cab85aa43f
Fernando Rodrigues Morais <150272982+Tabolito@users.noreply.github.com>
2025-05-27 08:29:12 -0300

    Update Fase1.js

--- src/scenes/Fase1.js
+++ src/scenes/Fase1.js
@@ -4,25 +4,24 @@ export class Fase1 extends Phaser.Scene {
   }
 
   preload() {
-        this.load.image('Floresta', 'assets/Floresta.jpeg');
+        this.load.image('Floresta', 'assets/Floresta.png');
         this.load.image('FlorestaChao', 'assets/FlorestaChao.png');
         this.load.atlas('magoAtlas', 'assets/sprites/AndarDoMago.png', 'assets/sprites/AndarDoMago.json');
         this.load.image('FlorestaTitulo', 'assets/aFlorestaPerdida.png')
-        this.load.atlas('magoPulo', 'assets/sprites/PuloDoMago.png', 'assets/sprites/PuloDoMago.json')
-
+        this.load.image('fireball', 'assets/FIREBALL.png');
+        // this.load.atlas('magoAtaque', 'assets/sprites/AtaqueDoMago.png', 'assets/sprites/AtaqueDoMago.json');
     }
 
   create() {
-
-
-            this.background = this.add.tileSprite(400, 300, 800, 600, 'Floresta');
-
+            
             const fase2 = this.add.image(400, 100, 'FlorestaTitulo');
             fase2.setScale(0.3);
 
 
             this.chao = this.physics.add.staticImage(400, 300, 'FlorestaChao')
          
+            this.background = this.add.tileSprite(400, 300, 800, 600, 'Floresta');
+
 
 
 
@@ -39,28 +38,44 @@ export class Fase1 extends Phaser.Scene {
                 repeat: -1
             });
 
-             this.anims.create({
-                key: 'PuloMago',
-                frames: this.anims.generateFrameNames('magoPulo', {
-                    start: 1,
-                    end: 5,
-                    zeroPad: 0,
-                    prefix: 'PuloDoMago ',
+            this.player = this.physics.add.sprite(100, 450,'magoAtlas', 'AndarDoMago 0.aseprite'); // nome da imagem carregada no preload
+
+
+            this.anims.create({
+                key: 'ataqueMago',
+                frames: this.anims.generateFrameNames('magoAtaque', {
+                    start: 0,
+                    end: 2,
+                    prefix: 'AtaqueDoMago ',
                     suffix: '.aseprite'
                 }),
                 frameRate: 5,
                 repeat: 0
             });
 
+            
+            this.estaAtacando = false;
 
-            this.player = this.physics.add.sprite(100, 450,'magoAtlas', 'AndarDoMago 0.aseprite'); // nome da imagem carregada no preload
+            
+            this.fireballs = this.physics.add.group({
+                classType: Phaser.Physics.Arcade.Image,
+                //maxSize: 10, ~ Limite das bolas de fogo
+                runChildUpdate: true
+            });
+
+            this.physics.world.on('worldbounds', (body) => {
+                if (body.gameObject && body.gameObject.texture.key === 'fireball') {
+                    body.gameObject.disableBody(true, true);
+                }
+            });
+
 
             this.player.setScale(2);
 
             this.player.setDebug(true);
 
-            this.player.setSize(42, 42);  // Largura, Altura (ajuste conforme seu sprite)
-            this.player.setOffset(22, 32); // Deslocamento X, Y (ajuste conforme necessário)
+            this.player.setSize(42, 60);  // Largura, Altura (ajuste conforme seu sprite)
+            this.player.setOffset(0, 14); // Deslocamento X, Y (ajuste conforme necessário)
             
             
             this.player.setDebug(true);
@@ -68,6 +83,8 @@ export class Fase1 extends Phaser.Scene {
             this.physics.add.collider(this.player, this.chao);
 
             this.cursors = this.input.keyboard.createCursorKeys();
+
+            this.attackKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
 
             this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
 
@@ -93,42 +110,47 @@ export class Fase1 extends Phaser.Scene {
             // Resetar movimento horizontal
             this.player.setVelocityX(0);
 
-            // Movimento lateral
             if (this.cursors.left.isDown) {
                 this.player.setVelocityX(-speed);
                 this.player.setFlipX(true);
+                this.player.setOffset(20, 14); // ajusta para flip esquerda
                 moving = true;
             } else if (this.cursors.right.isDown) {
                 this.player.setVelocityX(speed);
                 this.player.setFlipX(false);
+                this.player.setOffset(0, 14); // ajusta para flip direita
                 moving = true;
             }
 
             if (Phaser.Input.Keyboard.JustDown(this.spaceKey) && this.player.body.onFloor()) {
-                this.player.setVelocityY(-400);
-                this.player.play('PuloMago', true); // Força a animação imediatamente
+                this.player.setVelocityY(-500);
+                this.player.setOffset(7, 14)
+                
             }
-            
-            // Se está NO AR, toca animação de pulo
-            if (!this.player.body.onFloor) {
-                if (this.player.anims.getName() !== 'PuloMago') {
-                this.player.play('PuloMago', true);
-                    }
-            return;
-            // interrompe o resto
-            } 
 
-            // Se está no chão:
+            if (Phaser.Input.Keyboard.JustDown(this.attackKey)) {
+            this.atacar();
+            }
+
+
+        
+
+                        
+            if (!this.estaAtacando) {
             if (moving) {
                 if (this.player.anims.getName() !== 'andarMago') {
                 this.player.play('andarMago', true);
+                
                 }
             } else {
+                this.player.anims.stop();
                 this.player.setFrame('AndarDoMago 0.aseprite');
             }
+        }
 
 
-            // Verifica se o player chegou no limite d  ireito
+
+            // Verifica se o player chegou no limite direito
             if (this.player.x >= this.cameras.main.width) {
                 
                 this.comecarTransicaoParaFase2();
@@ -154,4 +176,33 @@ export class Fase1 extends Phaser.Scene {
                 });
             });
     }
+
+
+
+        atacar() {
+            if (this.estaAtacando) return ; // Evita atacar de novo durante a animação
+            this.player.setOffset(10, 25)
+            this.estaAtacando = true;
+            this.player.play('ataqueMago', true);
+
+
+            const fireball = this.fireballs.get();
+            if (fireball) {
+                fireball.setTexture('fireball');
+                fireball.enableBody(true, this.player.x, this.player.y - (-10), true, true);
+                fireball.setVelocityX(this.player.flipX ? -400 : 400);
+                fireball.body.setAllowGravity(false);
+                fireball.setCollideWorldBounds(true);
+                fireball.body.onWorldBounds = true;
+            }
+
+            // Quando a animação terminar, libera movimento
+            this.player.once(Phaser.Animations.Events.ANIMATION_COMPLETE, () => {
+                this.estaAtacando = false;
+            });
+     }
+
+     
+
 }
+
